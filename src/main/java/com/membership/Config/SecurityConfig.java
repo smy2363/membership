3ab525bbf29410f461ad107de332483a5be3e1dc
Beominator <woqja87@gmail.com>
2024-09-04 09:41:40 +0900

    0904

--- src/main/java/com/membership/Config/SecurityConfig.java
+++ src/main/java/com/membership/Config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
             //인가,인증 ,  누구든 접근 허용주소 설정
             http.authorizeRequests()
                     .mvcMatchers("/", "/member/**").permitAll()
-                    .mvcMatchers("/css/**","/js/**").permitAll()
+                    .mvcMatchers("/css/**","/js/**","/image/**").permitAll()
                     .mvcMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated();
 
