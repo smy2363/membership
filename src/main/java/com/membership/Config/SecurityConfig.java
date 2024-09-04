56e5e4a69786818e2f129ce1a4c5a09384d8e1a4
Beominator <woqja87@gmail.com>
2024-09-04 09:29:28 +0900

    0904

--- src/main/java/com/membership/Config/SecurityConfig.java
+++ src/main/java/com/membership/Config/SecurityConfig.java
@@ -1,21 +1,56 @@
 package com.membership.Config;
 
+import com.membership.Service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    MemberService memberService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.formLogin().disable(); // 기본 로그인 페이지 비활성화
-        http.csrf().disable(); // csrf토큰 비활성화
+            http.formLogin()
+                    .loginPage("/member/signIn")
+                    .defaultSuccessUrl("/")
+                    .usernameParameter("userId")
+                    .passwordParameter("password")
+                    .failureUrl("/member/signIn/error")
+                    .and()
+                    .logout()
+                    .logoutRequestMatcher( new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/");
+
+            //인가,인증 ,  누구든 접근 허용주소 설정
+            http.authorizeRequests()
+                    .mvcMatchers("/", "/member/**").permitAll()
+                    .mvcMatchers("/css/**","/js/**").permitAll()
+                    .mvcMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().authenticated();
+
+            http.csrf()
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+//        http.formLogin().disable(); // 기본 로그인 페이지 비활성화
+//        http.csrf().disable(); // csrf토큰 비활성화
 
 
        return http.build();
     }
 }
+
--- src/main/java/com/membership/Control/MemberControl.java
+++ src/main/java/com/membership/Control/MemberControl.java
@@ -4,6 +4,7 @@ import com.membership.Dto.MemberForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,9 +26,9 @@ public class MemberControl {
         return "member/findPw";
     }
 
-    @GetMapping("/signUp")
+    @PostMapping("/signUp")
     public String signUp(Model model){
         model.addAttribute("memberForm",new MemberForm());
-        return "member/signUp";
+        return "member/login";
     }
 }
