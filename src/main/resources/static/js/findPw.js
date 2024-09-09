3d22cabe8c17e1b5678d2c3410a0a38030b189ac
smy2363 <smy2363@naver.com>
2024-09-09 14:51:47 +0900

    0909

--- src/main/resources/static/js/findPw.js
+++ src/main/resources/static/js/findPw.js
@@ -1,13 +1,53 @@
 $(function(){
-    $("#find-pw-bt").click(function(){
-        alert("임시 비밀번호 발급");
-        findPw();
+    $("#confirmBtn").click(function(){
+        confirmNumber();
+    });
+
+    $("#sendBtn").click(function(){
+        sendNumber();
     });
 
 
-
-
 });
+function sendNumber(){
+    var token = $("meta[name=_csrf]").attr("content");
+    var header = $("meta[name=_csrf_header]").attr("content");
+    $("#mail_number").css("display", "block");
+        $.ajax({
+            url:"/mail",
+            type:"post",
+            dataType:"json",
+            data:{"mail" : $("#email").val()},
+            beforeSend : function(xhr){
+                        xhr.setRequestHeader(header, token);
+            },
+            success:function(data){
+                alert("인증번호 발송");
+                $("#Confirm").attr("value", data);
+            },
+            error:function(){
+                alert("인증번호 발송실패");
+                console.log($("#email").val());
+            }
+
+        });
+}
+
+function confirmNumber(){
+    var number1 = $("#emailCode").val();
+    var number2 = $("#Confirm").val();
+
+    if(number1 == number2){
+        alert("인증성공");
+        $("#find-pw-bt").click(function(){
+            alert("임시 비밀번호 발급");
+            findPw();
+        });
+    }else{
+        alert("인증실패");
+        $("#find-pw-bt").off('click');
+    }
+}
 
 function findPw(){
     var token = $("meta[name=_csrf]").attr("content");
@@ -28,7 +68,6 @@ function findPw(){
                 error:function(){
                     alert("이메일 발송실패");
                 }
-
     });
 
 }
